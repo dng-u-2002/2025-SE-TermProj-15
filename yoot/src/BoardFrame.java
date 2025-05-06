1d8477fb07fd592b9440f592f8a9e2a6577763c7
cauyoon <williamha13@gmail.com>
2025-05-06 14:56:39 +0900

    java start
    environment setting

---
+++ yoot/src/BoardFrame.java
@@ -0,0 +1,10 @@
+
+
+public class BoardFrame extends JFrame {
+    BoardFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
---
+++ yoot/src/MainFrame.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.swing.*;
+public class MainFrame {
+    public static void main(String[] args) {
+
+        new BoardFrame();
+
+    }
+}
