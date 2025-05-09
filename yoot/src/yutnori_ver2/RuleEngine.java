d82972d94ac9d3a8cc35ceb4db496a6e3f463917
cauyoon <williamha13@gmail.com>
2025-05-09 21:17:29 +0900

    프론트와 호환성 맞춤
    - 확장성에 적합하게 데이터값 수정
    - 코드 경량화
    but, 디버깅 전...

---
+++ yoot/src/yutnori_ver2/RuleEngine.java
@@ -0,0 +1,125 @@
+package yutnori_ver2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuleEngine {
+    private int index;
+    private int boardType;
+    private List<Integer> yutResult;
+    public List<Integer> possibleLocation = new ArrayList<>();
+    private boolean isCorner = false;
+    private boolean isCenter = false;
+    private boolean isDiagonal = false;
+    public boolean isFinish = false;
+
+    public RuleEngine(int index, int boardType, List<Integer> yutResult){
+        this.index = index;
+        this.boardType = boardType;
+        this.yutResult = yutResult;
+        if(index == 50*boardType){
+            isCenter = true;
+        } else if(index % 5 == 0){
+            isCorner = true;
+        } else if(index >= 50){
+            isDiagonal = true;
+        }
+
+    }
+
+    public List<Integer> getPossibleLocation(){
+        for(int yut: yutResult){
+            if(isCenter){
+                switch(yut){
+                    case -1 -> possibleLocation.add((boardType-1) * 50 + 2 ); //빽도 구현 미완성 지점
+                    case 1, 2 -> possibleLocation.add(index + yut);
+                    case 3 -> possibleLocation.add(0);
+                    case 4, 5 -> isFinish = true;
+                }
+            } else if(index == 0){
+                switch(yut){
+                    case -1 -> possibleLocation.add(boardType*50 - 1);
+                    case 1, 2, 3, 4, 5 -> isFinish = true;
+                }
+            } else if(isCorner){        //일단 가운데로 가도록 만들어둠
+                switch(yut){
+                    case -1 -> possibleLocation.add(index + yut);
+                    case 1, 2 -> possibleLocation.add(index*10 + yut);
+                    case 3 -> possibleLocation.add(boardType*50);
+                    case 4 -> possibleLocation.add(boardType*50 + 1);
+                    case 5 -> possibleLocation.add(boardType*50 + 2);
+                }
+            } else if(isDiagonal){
+                int temp = index % 10;
+                if(temp == 1 && index < boardType*50){
+                    switch(yut){
+                        case -1 -> possibleLocation.add((index + yut) / 10);
+                        case 1 -> possibleLocation.add(index + yut);
+                        case 2 -> possibleLocation.add(boardType*50);
+                        case 3 -> possibleLocation.add(boardType*50 + 1);
+                        case 4 -> possibleLocation.add(boardType*50 + 2);
+                        case 5 -> possibleLocation.add(0);
+                    }
+                } else if(temp == 2 && index < boardType*50){
+                    switch(yut){
+                        case -1 -> possibleLocation.add(index + yut);
+                        case 1 -> possibleLocation.add(boardType*50);
+                        case 2 -> possibleLocation.add(boardType*50 + 1);
+                        case 3 -> possibleLocation.add(boardType*50 + 2);
+                        case 4 -> possibleLocation.add(0);
+                        case 5 -> isFinish = true;
+                    }
+                } else if(temp == 1 && index > boardType*50){
+                    switch(yut){
+                        case -1, 1 -> possibleLocation.add(index + yut);
+                        case 2 -> possibleLocation.add(0);
+                        case 3, 4, 5 -> isFinish = true;
+                    }
+                } else if(temp == 2 && index > boardType*50){
+                    switch(yut){
+                        case -1 -> possibleLocation.add(index + yut);
+                        case 1 -> possibleLocation.add(0);
+                        case 2, 3, 4, 5 -> isFinish = true;
+                    }
+                }
+            } else {
+                if(index + yut >= boardType*50){
+                    isFinish = true;
+                } else{
+                    possibleLocation.add(index + yut);
+                }
+            }
+        }
+        for(int i = 0; i < possibleLocation.size(); i++){
+            System.out.println((i+1) + ". possible location: " + possibleLocation.get(i));
+        }
+        return possibleLocation;
+    }
+
+    public boolean finishAble(int yut){
+        if(isCenter && (yut == 4 || yut == 5)){
+            return true;
+        } else if(index == 0 && yut!= -1){
+            return true;
+        } else if(isDiagonal){
+            int temp = index % 10;
+            if(temp == 2 && index < boardType*50){
+                if(yut == 5){
+                    return true;
+                }
+            } else if(temp == 1 && index > boardType*50){
+                if(yut == 3 || yut == 4 || yut == 5){
+                    return true;
+                }
+            } else if(temp == 2 && index > boardType*50){
+                if(yut == 2 ||yut == 3 || yut == 4 || yut == 5){
+                    return true;
+                }
+            }
+        } else {
+            if(index + yut >= boardType*50){
+                return true;
+            }
+        }
+        return false;
+    }
+}
