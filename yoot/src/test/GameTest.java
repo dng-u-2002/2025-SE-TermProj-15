b085cb001d9c97e1d7b20561cf05f301091d288f
youngjun <leo5852@naver.com>
2025-05-23 17:34:01 +0900

    -getPossibleLocation Test 작성중

---
+++ yoot/src/test/GameTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void updateGameStatus() {
+
+    }
+
+    @Test
+    void throwYutButtonClicked() {
+    }
+
+    @Test
+    void pieceSelected() {
+    }
+
+    @Test
+    void moveLocationSelected() {
+    }
+
+    @Test
+    void restartGame() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getCurrentState() {
+    }
+}
---
+++ yoot/src/test/RuleEngineTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleEngineTest {
+    RuleEngine ruleEngine;
+    List<Integer> yutResult = new ArrayList<Integer>();
+    @BeforeAll
+    static void init() {
+
+    }
+    @BeforeEach
+    void setUp() {
+        yutResult.add(1);
+        ruleEngine = new RuleEngine(0, 4, yutResult);
+    }
+    @Test
+    void getPossibleLocationTest() {
+        List<Integer> expected = new ArrayList<Integer>(){{add(1);}};
+        //assertIterableEquals(ruleEngine.getPossibleLocation(), expected);
+        assertEquals(expected.get(0), ruleEngine.getPossibleLocation().get(0));
+    }
+
+    @Test
+    void finishAbleTest() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.gc();
+    }
+}
