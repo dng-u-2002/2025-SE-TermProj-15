1603e809ea4d17129f9c4934ed27eda9a87aa67c
leo5852-1 <leokyj5852@gmail.com>
2025-06-03 14:15:27 +0900

    -Junit test added

---
+++ yoot/src/test/YutResultTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class YutResultTest {
+    @Test
+    public void testValueMatchesType() {
+        assertEquals(-1, new YutResult(YutResultType.BACK_DO).getValue());
+        assertEquals(1, new YutResult(YutResultType.DO).getValue());
+        assertEquals(2, new YutResult(YutResultType.GAE).getValue());
+        assertEquals(3, new YutResult(YutResultType.GEOL).getValue());
+        assertEquals(4, new YutResult(YutResultType.YUT).getValue());
+        assertEquals(5, new YutResult(YutResultType.MO).getValue());
+    }
+
+    @Test
+    public void testTypeIsStoredCorrectly() {
+        YutResult result = new YutResult(YutResultType.DO);
+        assertEquals(YutResultType.DO, result.getType());
+    }
+
+    @Test
+    public void testDisplayNames() {
+        assertEquals("빽도", YutResultType.BACK_DO.getDisplayName());
+        assertEquals("도", YutResultType.DO.getDisplayName());
+        assertEquals("개", YutResultType.GAE.getDisplayName());
+        assertEquals("걸", YutResultType.GEOL.getDisplayName());
+        assertEquals("윷", YutResultType.YUT.getDisplayName());
+        assertEquals("모", YutResultType.MO.getDisplayName());
+    }
+
+}
---
+++ yoot/src/test/YutThrowerTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class YutThrowerTest {
+    @Test
+    public void testThrowRandomResultVaild() {
+        YutResult result = YutThrower.throwRandom();
+        assertNotNull(result, "Random Throw 결과는 null이 될 수 없음.");
+        assertNotNull(result.getType(), "Random Throw 결과의 타입은 null이 될 수 없음.");
+        int resultValue = result.getValue();
+        assertTrue((1 <= resultValue && resultValue <= 5) || resultValue == -1, "Random Throw 결과의 value는 -1 또는 1~5의 값이여야 함.");
+    }
+    @Test
+    public void testThrowRandomResultDistribution() {
+        HashSet<Integer> resultSet = new HashSet<>();
+
+        for (int i = 0; i < 10000; i++) {
+            YutResult result = YutThrower.throwRandom();
+            resultSet.add(result.getValue());
+        }
+
+        assertTrue(resultSet.contains(-1),  "빽도가 1000번의 시행 중 한 번도 등장하지 않았음.");
+        for (int i = 1; i <= 5; i++) {
+            assertTrue(resultSet.contains(i), YutResultType.values()[i].getDisplayName() + "(이)가 1000번의 시행 중 한 번도 등장하지 않았음.");
+        }
+    }
+
+    @Test
+    public void testThrowManualResultVaild() {
+
+    }
+
+}
